2ff1175677b7f3e92acacf9c5b486077e89a5133
MTN73 <87441196+MTN73@users.noreply.github.com>
2023-11-13 22:37:36 -0500

    Add ultrasonic sensor test
    sends distance to serial monitor (9600)

--- motorShieldTestNEW.ino
+++ motorShieldTestNEW.ino
@@ -1,18 +1,34 @@
 /*
 for Keyestudio Turtle Robot V3.1 :
 
-tests both drive motors and servo
+tests both drive motors, servo, and ultrasonic sensor (sent to serial monitor)
 */
 
 #include <Servo.h>
 Servo servo;
+
 #define ultrasonicTrigPin 12
 #define ultrasonicEchoPin 13
 #define ultrasonicServoPin A3
+
 #define motorA_direction 9     //direction control pin of A motor / L
 #define motorA_PWM 2   //PWM control pin of A motor
 #define motorB_direction 4    //direction control pin of B motor
 #define motorB_PWM 5   //PWM control pin of B motor / R
+
+float distance_cm;
+
+void getDistance(){
+  digitalWrite(ultrasonicTrigPin,LOW);
+  delayMicroseconds(2);
+  digitalWrite(ultrasonicTrigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ultrasonicTrigPin,LOW);
+  duration = pulseIn(ultrasonicEchoPin,HIGH);
+  distance_cm = (34/duration)/2; //2*distance = (speed/time)
+  Serial.println(distance_cm);
+}
+
 void setup(){
   pinMode(ultrasonicTrigPin,OUTPUT);
   pinMode(ultrasonicEchoPin,OUTPUT);
@@ -22,6 +38,8 @@ void setup(){
   pinMode(motorB_PWM,OUTPUT);
   pinMode(ultrasonicServoPin,OUTPUT);
   servo.attach(ultrasonicServoPin);
+
+  Serial.begin(9600);
 }
 void loop(){
   //servo.write(150);
@@ -29,4 +47,6 @@ void loop(){
   analogWrite(motorA_PWM,220);
   digitalWrite(motorB_direction,HIGH);
   analogWrite(motorB_PWM,220);
+
+  getDistance();
 }

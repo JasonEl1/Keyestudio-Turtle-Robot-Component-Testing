053466d7ad15b50e3e91323d5f34a10b480b7071
JasonEl1 <87441196+JasonEl1@users.noreply.github.com>
2023-12-13 20:43:46 -0500

    Add 8x8 matrix testing

--- motorShieldTestNEW.ino
+++ motorShieldTestNEW.ino
@@ -1,8 +1,20 @@
 /*
 for Keyestudio Turtle Robot V3.1 :
 
-tests both drive motors, servo, and ultrasonic sensor.
+tests both drive motors, servo, 8x8 i2c matrix, and ultrasonic sensor.
+
+requires Adafruit_LEDBackpack and Adafruit_GFX libraries. 
+LEDBackpack download : https://drive.google.com/drive/folders/1Fg4iyz1GAwLuM2Pid2zuK3o0rjIoWdiZ
+GFX download : https://drive.google.com/drive/folders/1EWay8sCtzs7juY7WWuEg3I3fRTOku-HT
+place downloaded library folders in "libraries" sub-folder of Arduino folder
+
+or install using arduino IDE manage libraries tool
 */
+
+#include <Wire.h>
+#include "Adafruit_LEDBackpack.h"
+#include "Adafruit_GFX.h"
+Adafruit_LEDBackpack matrix = Adafruit_LEDBackpack();
 
 #include <Servo.h>
 Servo servo;
@@ -15,6 +27,8 @@ Servo servo;
 #define motorA_PWM 2   //PWM control pin of A motor
 #define motorB_direction 4    //direction control pin of B motor
 #define motorB_PWM 5   //PWM control pin of B motor / R
+
+//8x8 matrix : A5-SCL, A4-SDA
 
 float distance_cm;
 long duration;
@@ -31,6 +45,30 @@ void getDistance(){
   if(distance_cm>0.0){Serial.println(distance_cm);}
 }
 
+void matrix_smile(){
+  matrix.displaybuffer[7] = B00000000;
+  matrix.displaybuffer[6] = B00100100;
+  matrix.displaybuffer[5] = B00100100;
+  matrix.displaybuffer[4] = B00100100;
+  matrix.displaybuffer[3] = B00000000;
+  matrix.displaybuffer[2] = B01000010;
+  matrix.displaybuffer[1] = B01111110;
+  matrix.displaybuffer[0] = B00000000;
+  matrix.writeDisplay();
+}
+
+void matrix_clear(){
+  matrix.displaybuffer[0] = B00000000;
+  matrix.displaybuffer[1] = B00000000;
+  matrix.displaybuffer[2] = B00000000;
+  matrix.displaybuffer[3] = B00000000;
+  matrix.displaybuffer[4] = B00000000;
+  matrix.displaybuffer[5] = B00000000;
+  matrix.displaybuffer[6] = B00000000;
+  matrix.displaybuffer[7] = B00000000;
+  matrix.writeDisplay();
+}
+
 void setup(){
   pinMode(ultrasonicTrigPin,OUTPUT);
   pinMode(ultrasonicEchoPin,INPUT);
@@ -42,8 +80,12 @@ void setup(){
   servo.attach(ultrasonicServoPin);
 
   Serial.begin(9600);
+
+  matrix.begin(0x70); //default adress, can check for real adress using i2c scanner program if needed
 }
 void loop(){
+  matrix_smile();
+  
   servo.write(150);
   digitalWrite(motorA_direction,HIGH);
   analogWrite(motorA_PWM,220);
@@ -51,4 +93,8 @@ void loop(){
   analogWrite(motorB_PWM,220);
 
   getDistance();
+
+  delay(3000);
+  matrix_clear();
+  delay(1000);
 }
